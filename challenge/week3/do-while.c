ebd2b31aec0257c42d20d6ef8f8e2ebae51c0e31
이재현 <lcw7010@naver.com>
2023-09-18 16:44:19 +0900

    Week3.1반복문

--- challenge/week3/do-while.c
+++ challenge/week3/do-while.c
@@ -3,12 +3,12 @@
 int main() {
 	int i = 0;
 	do {
-		i++;
-		if (i % 2 == 0) {
+		i++; //i에 1추가
+		if (i % 2 == 0) { // i가 짝수일때 다시반복
 			continue;
 		}
 		printf("%d Hello World!\n", i);
 		
-	} while (i < 10);
+	} while (i < 10); // i는 10미만까지
 	return 0;
 }
--- challenge/week3/while_even.c
+++ challenge/week3/while_even.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 
 int main() {
-	int i = 0;
+	int i = 0; // i = 0이다
 	while (i < 10) {
-		i++;
-		if (i % 2 == 0) {
+		i++; // i에 먼저 1추가
+		if (i % 2 == 0) { // i가 짝수일때는 continue로 앞으로
 			continue;
 		}
 		printf("%d Hello World!\n", i);
