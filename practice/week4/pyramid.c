619a7c02a9c8ebc671f6e4318285faeb797494f5
이재현 <lcw7010@naver.com>
2023-09-25 15:40:38 +0900

    Week4.1. 반복문 실습 + 피라미드 쌓기

--- practice/week4/pyramid.c
+++ practice/week4/pyramid.c
@@ -1,18 +1,18 @@
 #include <stdio.h>
 
 int main() {
-	int floor;
-	printf("몇 층을 쌓겠습니까? (5~100)");
-	scanf_s("%d", &floor);
+	int floor; // floor라는 정수형 변수 
+	printf("몇 층을 쌓겠습니까? (5~100)"); // 층수를 물어본다.
+	scanf_s("%d", &floor); // floor의 변수에 숫자 입력
 
-	for (int i = 0; i < floor; i++) {
-		for (int j = 0; j < floor - 1 - i; j++) {
+	for (int i = 0; i < floor; i++) { // floor줄을 출력
+		for (int j = 0; j < floor - 1 - i; j++) { // floor줄 -1-i
 			printf(" "); // 공백이 더 이뻐서 S대신 공백으로 바꿨습니다.
 
 		}
-		for (int k = 0; k < i * 2 + 1; k++) {
+		for (int k = 0; k < i * 2 + 1; k++) { // *을 i*2+1만큼 출력
 			printf("*");
 		}
-		printf("\n");
+		printf("\n"); // 줄바꿈
 	}
 }
